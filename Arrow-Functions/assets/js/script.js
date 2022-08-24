256f1cd3c115407ab044a3c45dc85d5d372c4485
Khazanism <khazanyassin@gmail.com>
2022-08-24 22:42:32 +0200

    Arrow function

---
+++ Arrow-Functions/assets/js/script.js
@@ -0,0 +1,31 @@
+//  Arrow functions
+
+// function printMyName(name) {
+//   console.log(name);
+// }
+
+// printMyName("max");
+
+// const printMyName = (name) => {
+//   console.log(name);
+// };
+// printMyName("max");
+
+// const printMyName = () => {
+//   console.log("max");
+// };
+// printMyName();
+
+// const printMyName = (name, age) => {
+//   console.log(name, age);
+// };
+// printMyName("mo", 15);
+
+const multiply = (number) => {
+  return number * 2;
+};
+// OR you can write as follows 
+
+const multiply = number => number*2;
+
+console.log(multiply(2));

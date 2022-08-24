98cde89d52ab40305b12f336bcc4f3eedc52b403
Khazanism <khazanyassin@gmail.com>
2022-08-24 22:05:50 +0200

    let and const

--- let&const/assets/js/script.js
+++ let&const/assets/js/script.js
@@ -1,1 +1,11 @@
 //  let ans const
+
+// let is the new var use it for variable values.
+
+// const is value that you assign once and never change
+
+const myName = "max";
+console.log(myName);
+
+myName = "manu";
+console.log(myName); // doesn't work because we assign another value to const.

31b2519e34ea3920260fa1f0f34e58fcf2b139ef
Khazanism <khazanyassin@gmail.com>
2022-08-25 23:53:52 +0200

    Export and Import Statements

---
+++ Exports&Imports/assets/js/script.js
@@ -0,0 +1,12 @@
+//  Export & Import statements
+
+// person . js 
+const person = {
+    name:'max'
+}
+
+export default person
+
+// utility.js
+export const clean =() => {...}
+export const baseData = 10;

ddbdd0e40f0f02d0b751b59379e83fc9997ea3b5
Khazanism <khazanyassin@gmail.com>
2022-08-26 23:40:14 +0200

    super method

--- Classes/assets/js/script.js
+++ Classes/assets/js/script.js
@@ -7,14 +7,28 @@ class person {
 }
 */
 
-class person {
+class Human {
   constructor() {
+    this.gender = "male";
+  }
+
+  printGender() {
+    console.log(this.gender);
+  }
+}
+
+class Person extends Human {
+  constructor() {
+    super(); // key word to execute the parent constructor too
+    // to correctly initialize the parent class
     this.name = "Max";
+    this.gender = "female";
   }
   printMyName() {
     console.log(this.name);
   }
 }
 
-const person = new person();
+const person = new Person();
 person.printMyName();
+person.printGender();

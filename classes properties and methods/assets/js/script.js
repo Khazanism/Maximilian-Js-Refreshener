6b2af3b714e66f289156c028ab96c1aab72ea74b
Khazanism <khazanyassin@gmail.com>
2022-08-28 10:34:05 +0200

    Next Gen. Js

--- classes properties and methods/assets/js/script.js
+++ classes properties and methods/assets/js/script.js
@@ -1,2 +1,24 @@
 //  classes properties and methods
 
+//  classes properties and methods NEXT GENERATION JS
+
+class Human {
+  gender = "male";
+
+  printGender = () => {
+    console.log(this.gender);
+  };
+}
+
+class Person extends Human {
+  name = "Max";
+  gender = "female";
+
+  printMyName = () => {
+    console.log(this.name);
+  };
+}
+
+const person = new Person();
+person.printMyName();
+person.printGender();

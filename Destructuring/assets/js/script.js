51ff795218db24cc98a0da081b7fa4395681190d
Khazanism <khazanyassin@gmail.com>
2022-08-29 23:41:45 +0200

    Destructuring

---
+++ Destructuring/assets/js/script.js
@@ -0,0 +1,9 @@
+//  Destructuring
+
+// easily extract array elements or object properties and store them in variables
+
+// array Destructuring or object Destructuring
+
+const numbers = [1, 2, 3];
+[num1, ,num3] = numbers;
+console.log(num1, num3);
